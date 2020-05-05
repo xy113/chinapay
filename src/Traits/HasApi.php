9d3d043684b951cd7244416231926d8da1026c86
xy113 <songdewei@163.com>
2020-05-05 14:10:30 +0800

    移除第三方库

--- src/Traits/HasApi.php
+++ src/Traits/HasApi.php
@@ -15,7 +15,6 @@ namespace ChinaPay\Traits;
 
 
 use ChinaPay\Exception\ChinaPayException;
-use GuzzleHttp\Client;
 
 trait HasApi
 {
@@ -43,7 +42,7 @@ trait HasApi
     /**
      * @return array
      */
-    public function getContent(): array
+    public function getContent()
     {
         return $this->content;
     }
@@ -52,7 +51,7 @@ trait HasApi
      * @param array $content
      * @return HasApi
      */
-    public function setContent(array $content): HasApi
+    public function setContent(array $content)
     {
         $this->content = $content;
         return $this;
@@ -64,9 +63,9 @@ trait HasApi
      * @return string
      * @throws ChinaPayException
      */
-    public function sendRequest($content=[])
+    public function sendRequest($content = [])
     {
-        if (!empty($content)){
+        if (!empty($content)) {
             $this->content = $content;
         }
 
@@ -76,15 +75,17 @@ trait HasApi
             throw new ChinaPayException('missing Signature value', 400);
         }
 
-        $clinet = new Client();
-        $res = $clinet->post($this->api, [
-            'form_params' => $this->content
-        ]);
-        if ($res->getStatusCode() == 200) {
-            return $res->getBody()->getContents();
-        } else {
-            throw new ChinaPayException($res->getReasonPhrase(), $res->getStatusCode());
-        }
+        return $this->curlPost($this->api, $this->content);
+
+//        $clinet = new Client();
+//        $res = $clinet->post($this->api, [
+//            'form_params' => $this->content
+//        ]);
+//        if ($res->getStatusCode() == 200) {
+//            return $res->getBody()->getContents();
+//        } else {
+//            throw new ChinaPayException($res->getReasonPhrase(), $res->getStatusCode());
+//        }
     }
 
     /**
@@ -93,8 +94,9 @@ trait HasApi
      * @return string
      * @throws ChinaPayException
      */
-    public function buildRequestForm($content=[]){
-        if (!empty($content)){
+    public function buildRequestForm($content = [])
+    {
+        if (!empty($content)) {
             $this->content = $content;
         }
 
@@ -105,22 +107,24 @@ trait HasApi
         }
 
         $fields = '';
-        foreach ($this->content as $k=>$v){
-            $fields.= '<input type="hidden" name="'.$k.'" value="'.$v.'">';
+        foreach ($this->content as $k => $v) {
+            $fields .= '<input type="hidden" name="' . $k . '" value="' . $v . '">';
         }
-        return '<html><head><title>ChinaPay Form</title></head><body>'.
-            '<form method="post" id="form" action="'.$this->api.'">'.$fields.'</form>'.
+        return '<html><head><title>ChinaPay Form</title></head><body>' .
+            '<form method="post" id="form" action="' . $this->api . '">' . $fields . '</form>' .
             '<script>document.getElementById("form").submit();</script></body></html>';
     }
 
-    protected function validateContent(){
+    protected function validateContent()
+    {
 
     }
 
     /**
      * @throws ChinaPayException
      */
-    protected function validateCommonFields(){
+    protected function validateCommonFields()
+    {
         if (!isset($this->content['Version'])) {
             throw new ChinaPayException('missing Version value', 400);
         }
@@ -136,5 +140,27 @@ trait HasApi
         if (!isset($this->content['TranType'])) {
             throw new ChinaPayException('missing TranType value', 400);
         }
+    }
+
+    /**
+     * @param array $data
+     * @param int $ssl
+     * @param int $timeout
+     * @return mixed
+     */
+    protected function curlPost($url, $data = [], $ssl = true, $timeout = 500)
+    {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_TIMEOUT, $timeout);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, $ssl);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, $ssl);
+        $res = curl_exec($curl);
+        curl_close($curl);
+        return $res;
     }
 }
